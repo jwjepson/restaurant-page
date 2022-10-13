bbce65703269f2e2490db4f783d13a9554927b8e
Jake Jepson <jwjepson1@gmail.com>
2022-10-13 15:50:18 -0400

    add menu button to home

--- src/home.js
+++ src/home.js
@@ -6,6 +6,9 @@ function home() {
 
     const description = createHTMLElement("div", "description", "Serving Boston for over 50 years!");
     pageContent.appendChild(description);
+
+    const menuButton = createHTMLElement("button", "menu-button", "Menu");
+    description.appendChild(menuButton);
 }
 
 
