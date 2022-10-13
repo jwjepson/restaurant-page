e844cfcedc988dea7e564a0a8ff05b09644c7f03
Jake Jepson <jwjepson1@gmail.com>
2022-10-13 19:30:24 -0400

    add menu categories

--- src/menu.js
+++ src/menu.js
@@ -3,8 +3,15 @@ import {createHTMLElement, content} from "./index";
 function menu() {
     const mainContent = document.querySelector(".main-content");
     const menu = createHTMLElement("div", "menu", "Menu");
-
     mainContent.appendChild(menu);
-}
 
+    const categories = ["pizza", "pasta", "subs", "salads"];
+    categories.forEach((category) => {
+        const heading = createHTMLElement("div", `${category}-category`, category);
+        menu.appendChild(heading);
+    });
+
+    const pizzaCategory = document.querySelector(".pizza-category");
+
+}
 export {menu};
