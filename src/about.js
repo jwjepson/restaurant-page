0e1372a4411a6f4a18fc525c6349a93f42880a47
Jake Jepson <jwjepson1@gmail.com>
2022-10-13 17:12:14 -0400

    create content for about page

---
+++ src/about.js
@@ -0,0 +1,9 @@
+import {createHTMLElement, content} from "./index";
+
+function about() {
+    const mainContent = document.querySelector(".main-content");
+    const aboutContent = createHTMLElement("div", "about", "Boston Pizzeria is a family owned business that has been serving the community for over 50 years. We offer an array of pizzas, subs, pastas, and salads.");
+    mainContent.appendChild(aboutContent);
+}
+
+export {about};
