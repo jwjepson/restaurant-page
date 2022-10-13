dfaf3020d3e1ceb5e34a0e453634f29a044fe425
Jake Jepson <jwjepson1@gmail.com>
2022-10-13 17:08:33 -0400

    remove creating main content div

--- src/home.js
+++ src/home.js
@@ -1,9 +1,8 @@
 import {createHTMLElement, content} from "./index";
 
 function home() {
-    const pageContent = createHTMLElement("div", "main-content");
-    content.appendChild(pageContent);
-
+    
+    const pageContent = document.querySelector(".main-content");
     const description = createHTMLElement("div", "description", "Serving Boston for over 50 years!");
     pageContent.appendChild(description);
 
