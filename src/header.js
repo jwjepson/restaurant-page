fa4015a48b52c4a3936acaf6608ec6e2f0a48577
Jake Jepson <jwjepson1@gmail.com>
2022-10-13 12:40:07 -0400

    add nav bar to header

--- src/header.js
+++ src/header.js
@@ -4,8 +4,18 @@ import {createHTMLElement, content} from "./index";
 function header() {
     const header = createHTMLElement("div", "header");
     const title = createHTMLElement("div", "title", "Boston Pizzeria");
+
+    const nav = ["Home", "About", "Menu"];
+    const ul = createHTMLElement("ul", "nav");
+
+    nav.forEach((tab) => {
+        const li = createHTMLElement("li", "nav-item", tab);
+        ul.appendChild(li);
+    })
+    
     content.appendChild(header);
     header.appendChild(title);
+    header.appendChild(ul);
 }
 
 export {header};
