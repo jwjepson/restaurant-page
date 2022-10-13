d0bc00dbcc75b7a468ca41f434538683ec852402
Jake Jepson <jwjepson1@gmail.com>
2022-10-13 17:09:21 -0400

    clear page and load corresponding tab when clicked

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { header } from "./header";
 import { home } from "./home";
+import { about } from "./about";
 
 
 const content = document.querySelector("#content");
@@ -12,7 +13,30 @@ function createHTMLElement(type, id, text) {
     return element;
 }
 
+function clearPage() {
+    const mainContent = document.querySelector(".main-content")
+    mainContent.innerHTML = "";
+}
+
 header();
 home();
+
+const navItems = document.querySelectorAll(".nav-item");
+navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (item.textContent == "About") {
+            clearPage();
+            about();
+        }
+        else if (item.textContent == "Menu") {
+            alert("This is Menu Tab");
+        }
+        else {
+            clearPage();
+            home();
+        }
+    });
+});
+
 
 export { createHTMLElement, content };
