6c7df622ea216e4f084da5952825dec4902a51c6
Jake Jepson <jwjepson1@gmail.com>
2022-10-12 21:24:06 -0400

    import createHTMLElement function

--- src/home.js
+++ src/home.js
@@ -1,10 +1,9 @@
+import {createHTMLElement, content} from "./index";
+
+
 function home() {
-    const content = document.querySelector("#content");
-    const test = document.createElement("p");
-    test.classList.add("testing");
-    test.textContent = "This is a test";
-    
-    content.appendChild(test);
+    const title = createHTMLElement("div", "title", "This is a title");
+    content.appendChild(title);
 }
 
 export {home};
