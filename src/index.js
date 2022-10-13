b580d63b54b0c4834e228ebb572321c79102b1f1
Jake Jepson <jwjepson1@gmail.com>
2022-10-12 21:24:42 -0400

    createHTMLElement function & export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import { home } from "./home";
 
+
+const content = document.querySelector("#content");
+
+function createHTMLElement(type, id, text) {
+    const element = document.createElement(type);
+    element.classList.add(id)
+    element.textContent = text;
+    
+    return element;
+}
+
 home();
+
+export { createHTMLElement, content };
