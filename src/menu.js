72ea344ee99792b0f974ec36f615f18e96e7e90e
Jake Jepson <jwjepson1@gmail.com>
2022-10-13 23:21:15 -0400

    populate menu with items

--- src/menu.js
+++ src/menu.js
@@ -5,13 +5,59 @@ function menu() {
     const menu = createHTMLElement("div", "menu", "Menu");
     mainContent.appendChild(menu);
 
-    const categories = ["pizza", "pasta", "subs", "salads"];
+    const categories = ["Pizza", "Pasta", "Subs"];
     categories.forEach((category) => {
-        const heading = createHTMLElement("div", `${category}-category`, category);
-        menu.appendChild(heading);
+        const cat = createHTMLElement("div", `${category}-category`);
+        const heading = createHTMLElement("h2", "heading", category);
+        cat.appendChild(heading);
+        menu.appendChild(cat);
     });
 
-    const pizzaCategory = document.querySelector(".pizza-category");
+    const pizzaCategory = document.querySelector(".Pizza-category");
+    const pastaCategory = document.querySelector(".Pasta-category");
+    const subCategory = document.querySelector(".Subs-category");
 
+    const pizzas = {
+        "Cheese Pizza": "$12.50",
+        "Pepperoni Pizza": "$13.10"
+    };
+
+    for (const pizza in pizzas) {
+        const item = createHTMLElement("div", "item");
+        const name = createHTMLElement("div", "name", pizza);
+        const price = createHTMLElement("div", "price", pizzas[pizza]);
+        item.appendChild(name);
+        item.appendChild(price);
+        pizzaCategory.appendChild(item);
+    }
+
+    const pastas = {
+        "Chicken Alfredo": "$8.60",
+        "Pasta Primavera": "$11.50",
+        "Chicken Carbonara": "$12.10"
+    }
+
+    for (const pasta in pastas) {
+        const item = createHTMLElement("div", "item");
+        const name = createHTMLElement("div", "name", pasta);
+        const price = createHTMLElement("div", "price", pastas[pasta]);
+        item.appendChild(name);
+        item.appendChild(price);
+        pastaCategory.appendChild(item);
+    }
+
+    const subs = {
+        "Buffalo Chicken": "$7.50",
+        "Philly Cheese Steak": "$8.90",
+    }
+
+    for (const sub in subs) {
+        const item = createHTMLElement("div", "item");
+        const name = createHTMLElement("div", "name", sub);
+        const price = createHTMLElement("div", "price", subs[sub]);
+        item.appendChild(name);
+        item.appendChild(price);
+        subCategory.appendChild(item);
+    }
 }
 export {menu};
